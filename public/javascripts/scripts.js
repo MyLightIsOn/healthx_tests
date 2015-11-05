bb16602cbfd0e47f4b6d0b12615736cc4e59363c
Lawrence Moore <lawrence2978@hotmail.com>
2015-11-04 20:48:42 -0500

    Structured JSON for efficiency

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,6 +1,21 @@
 myApp = {
     init: function(){
-        console.log('Loaded!')
+        myApp.navOn();
+        myApp.primaryNav();
+    },
+
+    //Handle navigation animation
+    navOn: function(){
+        $('.menu-button').click(function(){
+            $('.primary-nav').toggleClass('nav-active');
+        });
+    },
+
+    primaryNav: function(){
+        $('.primary-nav-button').click(function(){
+            var selected = $(this);
+            /*console.log(selected[0].outerText)*/
+        });
     }
 };
 
